5bdadcf423a0d9a9122b0c462e8d5dd5e4930748
morningpee <morningpee2@gmail.com>
2016-06-16 11:22:04 -0500

    move startScriptFunction declaration

--- mtf.js
+++ mtf.js
@@ -35,6 +35,31 @@ function resizeContentFlash()
     $(contentFlash).css("margin-left", -updatedWidth / 2 + "px");
 }
 
+function startScriptFunction(){
+   gamePrerollComplete();
+   if(contentFlash.outerHTML.indexOf("object") == -1){
+       renderFlash();
+   }
+   if( location.href.indexOf("/Live/game.php") != -1 ){
+       var sArenaTimes = 5;
+       function startArena() {
+           if(contentFlash.TotalFrames)
+           {
+               try{
+                   contentFlash.as3_prerollDone();
+               }catch(err){}
+           }else
+           {
+               setTimeout(startArena, 1000);
+               return;
+           }
+           sArenaTimes--;
+           setTimeout(startArena, 1000);
+       };
+       startArena();
+   }
+}
+
 addEventListener("DOMContentLoaded",
     function()
     {
@@ -47,30 +72,6 @@ addEventListener("DOMContentLoaded",
         document.documentElement.innerHTML = 
             '<head>' + headStr + '</head>' +
             '<body>' + bodyStr + '</body>';
-        var startScriptFunction = function(){
-            gamePrerollComplete();
-            if(contentFlash.outerHTML.indexOf("object") == -1){
-                renderFlash();
-            }
-            if( location.href.indexOf("/Live/game.php") != -1 ){
-                var sArenaTimes = 5;
-                function startArena() {
-                    if(contentFlash.TotalFrames)
-                    {
-                        try{
-                            contentFlash.as3_prerollDone();
-                        }catch(err){}
-                    }else
-                    {
-                        setTimeout(startArena, 1000);
-                        return;
-                    }
-                    sArenaTimes--;
-                    setTimeout(startArena, 1000);
-                };
-                startArena();
-            }
-        }
         /*implicit typecast to get function scope contents as string*/
         var startScriptString = startScriptFunction + '';
         /*strip wrapping function declaration*/
