9952be8a83a5ef6a85af18a70eab170372ddc455
morningpee <morningpee2@gmail.com>
2016-09-06 02:39:33 -0500

    render in iframe

--- mtf.user.js
+++ mtf.user.js
@@ -14,42 +14,45 @@
 
 window.stop();
 
-flashVars = new Object();
-flashVars.apiUrl = "http://api.tetrisfriends.com/api";
-flashVars.startParam = "clickToPlay";
+function buildFlashVarsParamString()
+{
+    var flashVars = new Object();
+    flashVars.apiUrl = "http://api.tetrisfriends.com/api";
+    flashVars.startParam = "clickToPlay";
 
-var request = new XMLHttpRequest();
-var SYNCHRONOUS_REQUEST=false;
-request.open('GET', 'http://www.tetrisfriends.com/users/ajax/profile_my_tetris_style.php', SYNCHRONOUS_REQUEST);
-request.send(null);
+    var request = new XMLHttpRequest();
+    var SYNCHRONOUS_REQUEST=false;
+    request.open('GET', 'http://www.tetrisfriends.com/users/ajax/profile_my_tetris_style.php', SYNCHRONOUS_REQUEST);
+    request.send(null);
 
-if (request.status === 200) {
-    flashVars = Object.assign( flashVars, eval( request.responseText.match(/flashVars = {[\s\S]*timestamp.*}/)[0] ) );
-    delete flashVars.viewerId;
+    if (request.status === 200) {
+        flashVars = Object.assign( flashVars, eval( request.responseText.match(/flashVars = {[\s\S]*timestamp.*}/)[0] ) );
+        delete flashVars.viewerId;
+    }
+
+    flashVarsParamString = Object.keys( flashVars ).map(k => k + '=' + flashVars[k] ).join('&');
+    return flashVarsParamString;
 }
-
-flashVarsParamString = Object.keys( flashVars ).map(k => k + '=' + flashVars[k] ).join('&');
 
 var contentFlashSize = new Object();
 
-function talkAboutThatContentFlashSize()
+function talkAboutThatContentFlashSize(contentFlash)
 {
-    contentFlashSize.originalWidth = contentFlash.width;
-    contentFlashSize.originalHeight = contentFlash.height;
+    contentFlashSize.originalWidth = 760;
+    contentFlashSize.originalHeight = 560;
 
     contentFlashSize.scaleFactor = 2;
-    contentFlashSize.minimalWidth = contentFlash.width / contentFlashSize.scaleFactor;
-    contentFlashSize.minimalHeight = contentFlash.height / contentFlashSize.scaleFactor;
-
+    contentFlashSize.minimalWidth = 760 / contentFlashSize.scaleFactor;
+    contentFlashSize.minimalHeight = 560 / contentFlashSize.scaleFactor;
 }
 
-function setContentFlashSize()
+function setContentFlashSize(contentFlash)
 {
     contentFlash.style.width = contentFlashSize.minimalWidth + "px";
     contentFlash.style.height = contentFlashSize.minimalHeight + "px";
 }
 
-function transformContentFlash()
+function transformContentFlash(contentFlash)
 {
     var windowAspectRatio = innerHeight / innerWidth;
 
@@ -71,34 +74,46 @@ function transformContentFlash()
 
     scaleFactorX = updatedWidth / contentFlashSize.minimalWidth
     scaleFactorY = updatedHeight / contentFlashSize.minimalHeight;
-
     contentFlash.style.transform = "scale( " + scaleFactorX + " ) translate3d( -50%, -50%, 0px)";
 }
 
-contentFlash = document.createElement("embed");
-contentFlash.setAttribute("id", "contentFlash");
-contentFlash.setAttribute("allowscriptaccess", "always");
-contentFlash.setAttribute("name", "plugin");
-contentFlash.setAttribute("height", "560");
-contentFlash.setAttribute("width", "760");
-contentFlash.setAttribute("src", "http://www.tetrisfriends.com/data/games/Ultra/OWGameUltra.swf");
-contentFlash.setAttribute("type", "application/x-shockwave-flash");
-contentFlash.setAttribute("wmode", "opaque");
-contentFlash.setAttribute("flashvars", flashVarsParamString);
+function buildContentFlash(sourceDocument, flashVarsParamString)
+{
+    var contentFlash = sourceDocument.createElement("embed");
+    contentFlash.setAttribute("id", "contentFlash");
+    contentFlash.setAttribute("allowscriptaccess", "always");
+    contentFlash.setAttribute("name", "plugin");
+    contentFlash.setAttribute("src", "http://www.tetrisfriends.com/data/games/Ultra/OWGameUltra.swf");
+    contentFlash.setAttribute("type", "application/x-shockwave-flash");
+    contentFlash.setAttribute("wmode", "opaque");
+    contentFlash.setAttribute("flashvars", flashVarsParamString);
 
-talkAboutThatContentFlashSize();
+    talkAboutThatContentFlashSize(contentFlash);
+    setContentFlashSize(contentFlash);
+    transformContentFlash(contentFlash);
 
-contentFlash.removeAttribute("height");
-contentFlash.removeAttribute("width");
+    return contentFlash;
+}
 
-setContentFlashSize();
-transformContentFlash();
-addEventListener("resize", transformContentFlash);
+/* html5 */
+document.doctype&&
+    document.replaceChild( document.implementation.createDocumentType('html', "", ""), document.doctype );
 
-mtfDocumentElement = document.createElement("html");
-mtfDocumentElement.appendChild( document.createElement("head") ).innerHTML = '<meta name="viewport" content="height=100, initial-scale=1.0, minimum-scale=1.0, maximum-scale=1.0, user-scalable=no">' +
-                                                                             '<style> :root{ image-rendering: optimizespeed; } @viewport { zoom: 1; min-zoom: 1; max-zoom: 1; user-zoom: fixed; } body { background: url(//tetrisow-a.akamaihd.net/data5_0_0_1/images/bg.jpg) repeat-x; font-family: "Trebuchet MS",Helvetica,Tahoma,Geneva,Verdana,Arial,sans-serif; font-size: 12px; color: #666; margin: 0; text-align: left; display: block; overflow: hidden} #contentFlash { visibility: visible !important; position: absolute; top: 50%; left: 50%; transform-style: preserve-3d; transform-origin: top left; } * { margin: 0; padding: 0; outline: none; -moz-box-sizing: border-box; box-sizing: border-box; }</style>';
-mtfDocumentElement.appendChild( document.createElement("body") ).appendChild( contentFlash );
+mtfWrapper = document.createElement("html");
+mtfWrapper.appendChild( document.createElement("head") );
+mtfBody = mtfWrapper.appendChild( document.createElement("body") )
+    .appendChild(
+       mtfFrame = document.createElement("iframe")
+    );
+mtfBody.appendChild( document.createElement('style') ).innerHTML = '* { margin: 0; } iframe { border: 0; width: 100vw; height: 100vh; }';
+document.replaceChild( mtfWrapper, document.documentElement );
+mtfDocument = document.implementation.createHTMLDocument("Minimal Tetris Friends");
 
-document.removeChild( document.documentElement );
-document.appendChild( mtfDocumentElement );
+
+mtfDocument.head.innerHTML = '<meta name="viewport" content="height=100, initial-scale=1.0, minimum-scale=1.0, maximum-scale=1.0, user-scalable=no" />';
+mtfDocument.body.appendChild( document.createElement('style') ).innerHTML = ':root{ image-rendering: optimizespeed; } @viewport { zoom: 1; min-zoom: 1; max-zoom: 1; user-zoom: fixed; } * { margin: 0; padding: 0; outline: none; box-sizing: border-box; } body { background: url(http://tetrisow-a.akamaihd.net/data5_0_0_1/images/bg.jpg) repeat-x; margin: 0; display: block; overflow: hidden; } embed { position: absolute; top: 50vh; left: 50vw; transform-style: preserve-3d; transform-origin: top left; }';
+contentFlash = buildContentFlash( mtfDocument, buildFlashVarsParamString() );
+mtfDocument.body.appendChild( contentFlash );
+
+mtfFrame.src = "data:text/html," + mtfDocument.documentElement.outerHTML;
+addEventListener("resize", function(){ transformContentFlash.call( frames[0], frames[0].contentFlash ) } );
