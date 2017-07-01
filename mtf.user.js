4c1c685ca6f6fe12992f7d4fad9330d5c5dce3c1
morningpee <morningpee2@gmail.com>
2017-07-01 10:39:01 -0500

    innerHTML -> textContent, it's faster

--- mtf.user.js
+++ mtf.user.js
@@ -183,7 +183,7 @@ function mtfInit()
     }
 }
 
-document.body.appendChild( document.createElement('style') ).innerHTML = '* { margin: 0; } :root{ image-rendering: optimizespeed; } @viewport { zoom: 1; min-zoom: 1; max-zoom: 1; user-zoom: fixed; } * { margin: 0; padding: 0; outline: none; box-sizing: border-box; } body { background: url(http://tetrisow-a.akamaihd.net/data5_0_0_1/images/bg.jpg) repeat-x; margin: 0; display: block; overflow: hidden; } embed { position: absolute; top: 50%; left: 50%; }';
+document.body.appendChild( document.createElement('style') ).textContent = '* { margin: 0; } :root{ image-rendering: optimizespeed; } @viewport { zoom: 1; min-zoom: 1; max-zoom: 1; user-zoom: fixed; } * { margin: 0; padding: 0; outline: none; box-sizing: border-box; } body { background: url(http://tetrisow-a.akamaihd.net/data5_0_0_1/images/bg.jpg) repeat-x; margin: 0; display: block; overflow: hidden; } embed { position: absolute; top: 50%; left: 50%; }';
 
 buildFlashVarsParamString();
 
@@ -240,6 +240,6 @@ function haveFlashVars(responseText, flashVars)
 
     document.body.appendChild( buildContentFlash( flashVarsParamString ) );
     /* necessary on firefox to access contentFlash.PercentLoaded() */
-    document.body.appendChild( document.createElement("script") ).innerHTML = "(" + mtfInit + ")()";
+    document.body.appendChild( document.createElement("script") ).textContent = "(" + mtfInit + ")()";
 
 }
