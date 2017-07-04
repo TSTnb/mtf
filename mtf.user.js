dc8a5539125d3439d51442f58b9c752bcaf717a0
morningpee <morningpee2@gmail.com>
2017-07-04 14:25:45 -0500

    strict comparison

--- mtf.user.js
+++ mtf.user.js
@@ -75,7 +75,7 @@ function haveFlashVars(responseText, flashVars)
        var vars = query.split('&');
        for (var i=0;i<vars.length;i++) {
                var pair = vars[i].split('=');
-               if(pair[0] == parameter){return pair[1];}
+               if(pair[0] === parameter){return pair[1];}
        }
        return '';
     };
