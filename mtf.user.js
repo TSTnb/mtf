eac759296dff065c08cedc1ff773c69002ce3ec4
morningpee <morningpee2@gmail.com>
2017-07-05 07:04:39 -0500

    4.5.7: low flash quality

--- mtf.user.js
+++ mtf.user.js
@@ -9,7 +9,7 @@
 // @include http://*tetrisfriends.com/games/Live/game.php*
 // @grant none
 // @run-at document-end
-// @version 4.5.6
+// @version 4.5.7
 // @author morningpee
 // ==/UserScript==
 
