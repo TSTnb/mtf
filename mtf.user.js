d14322a3d0c13b6a08e9b809cb4e64c030f270ae
morningpee <morningpee2@gmail.com>
2017-07-05 05:27:17 -0500

    4.5.5: use existing contentFlash

--- mtf.user.js
+++ mtf.user.js
@@ -9,7 +9,7 @@
 // @include http://*tetrisfriends.com/games/Live/game.php*
 // @grant none
 // @run-at document-end
-// @version 4.5.4
+// @version 4.5.5
 // @author morningpee
 // ==/UserScript==
 
